6c8d0b987bb8f99a2d6f00d642c55dc9b32a825a
Ujjwal734 <ujjwalvishwakarma734@gmail.com>
2021-12-10 07:48:19 +0530

    Stack push pop

--- Stacksh/Program.cs
+++ Stacksh/Program.cs
@@ -1,3 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("My Stack programme");
+using Stacksh;
+//Console.WriteLine("My Stack programme");
+Stack myStack = new Stack();
+
+myStack.Push(56);
+myStack.Push(70);
+myStack.Push(30);
+myStack.DisplayStack();
+myStack.Peek();
+Console.WriteLine("Item popped from Stack : {0}", myStack.Pop());
+myStack.DisplayStack();
